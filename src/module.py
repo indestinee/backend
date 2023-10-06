7dd4152cd26473885483ca734932eac899b3a7eb
indestinee <indestinee@gmail.com>
2023-10-06 15:33:30 +0800

    update dao

--- src/module.py
+++ src/module.py
@@ -2,11 +2,10 @@ from sqlite_dao_ext import SqliteClient
 
 from src.config import ServerConfig
 from src.dao.unified_item_dao import UnifiedItemDao
-from src.data.unified_item import UnifiedItem
-from src.features.unified_item_data_loader import UnifiedItemDataLoader
 from src.features.cipher_supplier import CipherSupplier
 from src.features.ftp_supplier import FtpSupplier
 from src.features.logging_supplier import get_logger
+from src.features.unified_item_data_loader import UnifiedItemDataLoader
 
 config = ServerConfig()
 sqlite_client = SqliteClient(config.db_path)
